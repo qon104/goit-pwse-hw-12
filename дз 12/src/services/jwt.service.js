dbeb39583ab46aa199247d3a384d429dd84fab62
Кирилл <tajik4381@gmail.com>
2025-12-15 12:12:49 +0200

    .

--- дз 12/src/services/jwt.service.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateTokens = (payload) => {
-  const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET, { expiresIn: "15m" });
-  const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET, { expiresIn: "7d" });
-  return { accessToken, refreshToken };
-};
